66ed0495aef2af2089137f8ba05c5843c5b29bd7
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-01 11:32:29 -0300

    flutter: fab ok

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print("ok"),
+      ),
       appBar: AppBar(
         title: const Text('Minhas imagens'),
       ),
