4fc5868e350462d54b8cfb0c3b1ef15e291458da
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-11 12:58:46 -0300

    flutter: exibindo urls textualmente

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'models/image_model.dart';
+import 'widgets/image_list.dart';
 
 class App extends StatefulWidget {
   State<App> createState() {
@@ -10,8 +11,10 @@ class App extends StatefulWidget {
 }
 
 class AppState extends State<App> {
-  int numeroImagens = 0;
-  void obterImagem() {
+  int numeroImagens = 1;
+  List<ImageModel> imagens = [];
+  //async/await
+  void obterImagem() async {
     //uniform resource identifier
     //mailto:teste@email.om
     //tel:11991111393
@@ -21,7 +24,7 @@ class AppState extends State<App> {
       '/v1/search',
       {
         'query': 'people',
-        'page': '1',
+        'page': '$numeroImagens',
         'per_page': '1',
       },
     );
@@ -31,24 +34,25 @@ class AppState extends State<App> {
           '563492ad6f91700001000001e00b21ab6afb45a18c1d44a759556f14'
     });
     //IO-Bound (tem também CPU-Bound)
-    req.send().then((result) {
-      http.Response.fromStream(result).then((response) {
-        if (response.statusCode == 200) {
-          var decodedJSON = json.decode(response.body);
-          var imagem = ImageModel.fromJSON(decodedJSON);
-          print(imagem);
-        } else {
-          print("Deu errado: ${response.statusCode}");
-        }
+    final result = await req.send();
+    if (result.statusCode == 200) {
+      //converter de StreamedResponse para Response
+      final response = await http.Response.fromStream(result);
+      final decodedJSON = json.decode(response.body);
+      final imagem = ImageModel.fromJSON(decodedJSON);
+      print(imagem);
+      setState(() {
+        imagens.add(imagem);
+        numeroImagens++;
       });
-    });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Text('$numeroImagens'),
+        body: ImageList(imagens),
         floatingActionButton: FloatingActionButton(
           onPressed: obterImagem,
           child: const Icon(Icons.add_a_photo),
